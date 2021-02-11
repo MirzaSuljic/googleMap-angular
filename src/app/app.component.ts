ccb2b5979417c3d58830b9e284add8053c525452
MirzaSuljic <mirza.suljic@gmail.com>
2021-02-11 22:42:55 +0100

    creating google Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'googleMap-angular';
+  title = 'Angular Google Maps';
+
+  lat = 44.5375;
+  lng = 18.6735;
+
 }
